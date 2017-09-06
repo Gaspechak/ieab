a8d7df38399c826e8741e78f011f60b74f2c9ce9
xdovorak <31695450+xdovorak@users.noreply.github.com>
2017-09-06 08:42:29 -0400

    Adicionar componente footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,14 @@
+<template lang="html">
+  <div class="container">
+    <h4>Eventos</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Footer"
+}
+</script>
+
+<style lang="css">
+</style>
