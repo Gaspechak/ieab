956ddc9155ce034206a1333f4360e09b071c0c05
xdovorak <xdovorak@outlook.com.br>
2017-09-09 16:47:22 -0400

    Adicionado imagens da igreja

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -8,24 +8,25 @@
     <div id="cards" class="row text/center">
       <div class="col">
         <div class="card">
-          <img class="card-img-top" src="static/img/slide2.jpg">
+          <img class="card-img-top" src="static/img/igreja.jpg">
           <div class="card-body">
-            <h4 class="card-title">Poxoréu - MT</h4>
-            <p class="card-text">Ao fundo uma cartão postal de nossa cidade Morro da Mesa.</p>
+            <h4 class="card-title">Nossa sede em Poxoréu</h4>
+            <p class="card-text">Sejam bem-vindos a nossa igreja em Poxoréu.</p>
           </div>
         </div>
       </div>
       <div class="col">
         <div class="card">
-          <img class="card-img-top" src="static/img/slide2.jpg">
+          <img class="card-img-top" src="static/img/poxoreu.jpg">
           <div class="card-body">
-            <h4 class="card-title">Nossa sede em Poxoréu</h4>
-            <p class="card-text">Sejam vem-vindos a nossa igreja em Poxoréu.</p>
+            <h4 class="card-title">Poxoréu - MT</h4>
+            <p class="card-text">Ao fundo um cartão postal de nossa cidade Morro da Mesa.</p>
+
           </div>
         </div>
       </div>
     </div>
-    
+
   </div>
 </template>
 
@@ -48,15 +49,17 @@ export default {
 }
 
 .card {
-        margin: 0 auto; /* Added */
-        float: none; /* Added */
-        margin-bottom: 10px; /* Added */
+  margin: 0 auto;
+  /* Added */
+  float: none;
+  /* Added */
+  margin-bottom: 10px;
+  /* Added */
 }
 
-.card-img-top{
-  width:100%;
-  height:auto;
+.card-img-top {
+  width: 100%;
+  height: auto;
   min-width: 325px;
 }
-
 </style>
